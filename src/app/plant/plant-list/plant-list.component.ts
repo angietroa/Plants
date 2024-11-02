f248cc28be01660cd6c06b582ff7393252f01c65
Angie Roa <a.roap@uniandes.edu.co>
2024-11-01 23:44:38 -0500

    feat: add count by type

--- src/app/plant/plant-list/plant-list.component.ts
+++ src/app/plant/plant-list/plant-list.component.ts
@@ -5,17 +5,42 @@ import { PlantService } from "../plant.service";
 @Component({
   selector: "app-plant-list",
   templateUrl: "./plant-list.component.html",
+  styleUrls: ["./plant-list.component.css"],
 })
 export class PlantListComponent implements OnInit {
   plants: Array<Plant> = [];
   tableHeader: string[] = ["#", "Nombre común", "Tipo", "Clima"];
+  plantTotals = [
+    {
+      icon: "../../../assets/inside.svg",
+      label: "Total de plantas de interior",
+      total: 0,
+    },
+    {
+      icon: "../../../assets/outside.svg",
+      label: "Total de plantas de exterior",
+      total: 0,
+    },
+  ];
+  totalPlantsIndoor: number = 0;
+  totalPlantsOutdoor: number = 0;
 
   constructor(private plantService: PlantService) {}
 
   getPlants(): void {
     this.plantService.getPlants().subscribe((plants) => {
       this.plants = plants;
+      this.calculateTotals();
     });
+  }
+
+  calculateTotals(): void {
+    this.plantTotals[0].total = this.plants.filter(
+      (plant) => plant.tipo === "Interior"
+    ).length;
+    this.plantTotals[1].total = this.plants.filter(
+      (plant) => plant.tipo === "Exterior"
+    ).length;
   }
 
   ngOnInit() {
